d11e1297a77c921517e3aca6aedebe3d5fe99469
Francis Acheampong <francisacheampong@gmail.com>
2018-02-11 01:30:28 -0500

    commit txtlabel object

--- Scripts/Core/app.js
+++ Scripts/Core/app.js
@@ -1,20 +1,37 @@
 var canvas;
 var ctx;
-function gameLoop() {
-    requestAnimationFrame(gameLoop);
-    ctx.fillStyle = "black";
-    ctx.fillRect(0, 0, 640, 580);
-    ctx.save();
-    ctx.beginPath();
-    ctx.strokeStyle = "red";
-    ctx.lineWidth = 5;
-    ctx.arc(200, 200, 100, 0, 2 * Math.PI);
-    ctx.stroke();
-    ctx.restore();
-}
-window.onload = function () {
+var stage;
+var helloLabel;
+function Start() {
     canvas = document.getElementById("cnvs");
-    ctx = canvas.getContext("2d");
-    gameLoop();
+    stage = new createjs.Stage(canvas);
+    createjs.Ticker.framerate = 60;
+    createjs.Ticker.on("tick", Update);
+}
+function Update() {
+    stage.update();
+}
+function Main() {
+    console.log("try main");
+    helloLabel = new createjs.Text("Hello World!", "40px consolas", "#747949");
+    stage.addChild(helloLabel);
+}
+/*<function gameLoop():void {
+requestAnimationFrame(gameLoop);
+ctx.fillStyle="black";
+ctx.fillRect(0,0,640,580);
+
+ctx.save();
+ctx.beginPath();
+ctx.strokeStyle ="red";
+ctx.lineWidth=5;
+ctx.arc(200,200,100,0,2*Math.PI);
+ctx.stroke();
+ctx.restore();
+}*/
+window.onload = function () {
+    // canvas =<HTMLCanvasElement>document.getElementById("cnvs");
+    // ctx =  canvas.getContext("2d");
+    // gameLoop();
 };
 //# sourceMappingURL=app.js.map
--- Scripts/Core/app.ts
+++ Scripts/Core/app.ts
@@ -1,6 +1,7 @@
 let canvas:any;
-let ctx:CanvasRenderingContext2D;
+// let ctx:CanvasRenderingContext2D;
 let stage:createjs.Stage;
+let helloLabel:createjs.Text;
 
 function Start():void {
 canvas = document.getElementById("cnvs");
@@ -14,8 +15,12 @@ stage.update();
 }
 function Main(): void {
 console.log("try main");
+helloLabel = new createjs.Text("Hello World!","40px consolas","#000000");
+helloLabel.regX = helloLabel.getMeasuredWidth() *0.5;
+helloLabel.regY = helloLabel.getMeasuredHeight() * 0.5;
+stage.addChild(helloLabel);
 }
-function gameLoop():void {
+/*<function gameLoop():void {
 requestAnimationFrame(gameLoop);
 ctx.fillStyle="black";
 ctx.fillRect(0,0,640,580);
@@ -27,10 +32,10 @@ ctx.lineWidth=5;
 ctx.arc(200,200,100,0,2*Math.PI);
 ctx.stroke();
 ctx.restore();
-}
+}*/
 
 window.onload=()=> {
-canvas =<HTMLCanvasElement>document.getElementById("cnvs");
-ctx =  canvas.getContext("2d");
-gameLoop();
+// canvas =<HTMLCanvasElement>document.getElementById("cnvs");
+// ctx =  canvas.getContext("2d");
+ // gameLoop();
 };

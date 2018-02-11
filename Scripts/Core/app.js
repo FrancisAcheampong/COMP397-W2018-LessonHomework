c9f83683eff3f9b621bc9ca8a45f157f2c0c17c4
Francis Acheampong <francisacheampong@gmail.com>
2018-02-11 02:20:40 -0500

    last commit lesson 1

--- Scripts/Core/app.js
+++ Scripts/Core/app.js
@@ -1,19 +1,25 @@
 var canvas;
-var ctx;
+// let ctx:CanvasRenderingContext2D;
 var stage;
 var helloLabel;
 function Start() {
-    canvas = document.getElementById("cnvs");
+    canvas = document.getElementById("canvas");
     stage = new createjs.Stage(canvas);
     createjs.Ticker.framerate = 60;
     createjs.Ticker.on("tick", Update);
+    Main();
 }
 function Update() {
+    helloLabel.rotation += 10;
     stage.update();
 }
 function Main() {
     console.log("try main");
-    helloLabel = new createjs.Text("Hello World!", "40px consolas", "#747949");
+    helloLabel = new createjs.Text("Hello World!", "40px consolas", "#000000");
+    helloLabel.regX = helloLabel.getMeasuredWidth() * 0.5;
+    helloLabel.regY = helloLabel.getMeasuredHeight() * 0.5;
+    helloLabel.x = 320;
+    helloLabel.y = 240;
     stage.addChild(helloLabel);
 }
 /*<function gameLoop():void {
@@ -29,9 +35,10 @@ ctx.arc(200,200,100,0,2*Math.PI);
 ctx.stroke();
 ctx.restore();
 }*/
-window.onload = function () {
-    // canvas =<HTMLCanvasElement>document.getElementById("cnvs");
-    // ctx =  canvas.getContext("2d");
-    // gameLoop();
-};
+window.onload = Start;
+// window.onload=()=>Start; // {
+// canvas =<HTMLCanvasElement>document.getElementById("cnvs");
+// ctx =  canvas.getContext("2d");
+// gameLoop();
+// }; 
 //# sourceMappingURL=app.js.map
--- Scripts/Core/app.ts
+++ Scripts/Core/app.ts
@@ -4,20 +4,25 @@ let stage:createjs.Stage;
 let helloLabel:createjs.Text;
 
 function Start():void {
-canvas = document.getElementById("cnvs");
+canvas = document.getElementById("canvas");
 stage = new createjs.Stage(canvas);
 createjs.Ticker.framerate =60;
 createjs.Ticker.on("tick",Update);
+Main();
 }
 
 function Update(): void {
+    helloLabel.rotation += 10;
 stage.update();
 }
 function Main(): void {
 console.log("try main");
+
 helloLabel = new createjs.Text("Hello World!","40px consolas","#000000");
 helloLabel.regX = helloLabel.getMeasuredWidth() *0.5;
 helloLabel.regY = helloLabel.getMeasuredHeight() * 0.5;
+helloLabel.x = 320;
+helloLabel.y = 240;
 stage.addChild(helloLabel);
 }
 /*<function gameLoop():void {
@@ -33,9 +38,9 @@ ctx.arc(200,200,100,0,2*Math.PI);
 ctx.stroke();
 ctx.restore();
 }*/
-
-window.onload=()=> {
+window.onload = Start;
+// window.onload=()=>Start; // {
 // canvas =<HTMLCanvasElement>document.getElementById("cnvs");
 // ctx =  canvas.getContext("2d");
  // gameLoop();
-};
+// };
